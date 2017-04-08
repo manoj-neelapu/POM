310cc557ca509def9f52eab54ddcbaa24bb55f97
manoj-neelapu <manoj.neelapu@gmail.com>
2017-04-08 15:11:39 -0400

    Commented some packages
    Description - Commented some packages

--- src/org/pom/GoogleSearchTest.java
+++ src/org/pom/GoogleSearchTest.java
@@ -1,9 +1,9 @@
 package org.pom;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class GoogleSearchTest {
